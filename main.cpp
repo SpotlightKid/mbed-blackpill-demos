5e2050d525fb5f9f7a50952c6c3223bd2acbe68d
Christopher Arndt <chris@chrisarndt.de>
2021-01-04 02:33:58 +0100

    Implement joystick example firmware
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include "mbed.h"
 #include "mbed_events.h"
+#include "Joystick.h"
 #include "PinDetect.h"
+
+#define THRESHOLD 0.008
 
 DigitalOut  led1(LED_BLUE);
 PinDetect   btn(USER_BUTTON);
+Joystick    stk(A1, A4);
+
+Vector2D old_coord = {0.0f, 0.0f};
 
 void btn_pressed(void) {
     printf("Button pressed\r\n");
     led1 = !led1;
+}
+
+void read_joystick(void) {
+    Vector2D coord = stk.get_coord();
+    if (fabs(coord.x - old_coord.x) > THRESHOLD || fabs(coord.y - old_coord.y) > THRESHOLD) {
+        printf("(%d, %d)\n", (int)(coord.x * 512), (int)(coord.y * 512));
+        old_coord.x = coord.x;
+        old_coord.y = coord.y;
+    }
 }
 
 int main() {
@@ -18,5 +33,8 @@ int main() {
     btn.attach_asserted(queue->event(btn_pressed));
     btn.setSamplesTillAssert(3);
     btn.setSampleFrequency();
+
+    stk.init();
+    queue->call_every(20ms, read_joystick);
     queue->dispatch_forever();
 }
